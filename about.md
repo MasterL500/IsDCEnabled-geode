761179f56b916d4f927e95f1013b79d17a091f3b
ML5 <Fireluis500@gmail.com>
2024-07-11 14:42:47 -0400

    🚧 Starting Code

---
+++ about.md
@@ -0,0 +1,5 @@
+# Is "Disable Checkpoints" on?
+
+Checkpoints don't work for you in platformers?
+
+Very small mod that just displays a message on the Pause Menu whenever you have the "Disable Checkpoints" option enabled.
---
+++ src/main.cpp
@@ -0,0 +1,23 @@
+#include <Geode/Geode.hpp>
+#include <Geode/modify/PauseLayer.hpp>
+
+using namespace geode::prelude;
+
+class $modify(CustomPauseLayer, PauseLayer) {
+	void customSetup() {
+		PauseLayer::customSetup();
+
+		auto winSize = CCDirector::sharedDirector()->getWinSize();
+		auto GM = GameManager::sharedState();
+
+		log::debug("Disable Checkpoints: {}", GM->getGameVariable("0146"));
+
+		if(GM->getGameVariable("0146")){
+			auto checkpointsLabel = CCSprite::create("checkpointsText.png"_spr);
+			checkpointsLabel->setPosition({winSize.width / 2 + 224.f, winSize.height / 2 + 100.f});
+			checkpointsLabel->setID("disabled-checkpoints-label");
+
+			this->addChild(checkpointsLabel);
+		}
+	}
+};
